6882920a6aceb6fb34fc15c66e643804d74b9c8c
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-16 22:11:11 +0100

    Added 00496FCTRL3 task.

---
+++ 00496FCTRL3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int factorial(int number) {
+    int result = 1;
+    for (; number > 0; number--) {
+        result *= number;
+    }
+    return result;
+}
+
+int main() {
+    int testCount, num, factorialResult, lastDigit, secondLastDigit;
+    cin >> testCount;
+    for (int i = 0; i < testCount; i++) {
+        cin >> num;
+        if (num < 10) {
+            factorialResult = factorial(num);
+            lastDigit = factorialResult % 10;
+            factorialResult /= 10;
+            secondLastDigit = factorialResult % 10;
+            cout << secondLastDigit << " " << lastDigit << endl;
+        }
+        else {
+            cout << 0 << " " << 0 << endl;
+        }
+    }
+}
