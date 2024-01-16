0f80f248d8219b74b7d9adbef55cd277f193e93b
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-16 22:09:44 +0100

    Added 00438PRIME_T task.

---
+++ 00438PRIME_T.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrime(int number) {
+    if (number <= 1)
+        return false;
+    if (number == 2)
+        return true;
+    if (number % 2 == 0)
+        return false;
+    int limit = sqrt(number);
+    for (int i = 3; i <= limit; i += 2) {
+        if (number % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int testCount, num;
+    cin >> testCount;
+    for (int i = 0; i < testCount; i++) {
+        cin >> num;
+        if (isPrime(num))
+            cout << "TAK" << endl;
+        else
+            cout << "NIE" << endl;
+    }
+    return 0;
+}
