d2168446cb2ed4ec72cbcb2dffa9c001a8041640
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-16 22:06:13 +0100

    Added 00549RNO_DOD task.

---
+++ 00549RNO_DOD.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCount, elementCount, sum, number;
+    cin >> testCount;
+    for (int i = 0; i < testCount; i++) {
+        cin >> elementCount;
+        sum = 0;
+        for (int j = 0; j < elementCount; j++) {
+            cin >> number;
+            sum += number;
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
