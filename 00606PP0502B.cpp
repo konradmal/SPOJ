e477a4387e89d2445bfe907adfe632ea660dd8f4
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-17 22:11:13 +0100

    Added 00606PP0502B task.

---
+++ 00606PP0502B.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int test = 0; test < n; test++) {
+        int a;
+        cin >> a;
+        int* tab = new int[a];
+        for (int j = a - 1; j >= 0; j--) {
+            cin >> tab[j];
+        }
+        for (int j = 0; j < a; j++) {
+            cout << tab[j] << " ";
+        }
+        cout << endl;
+        delete[] tab;
+    }
+}
