22c1887fdbdef1b7cdb1da85b6de296967296484
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-17 18:42:19 +0100

    Added 00601PP0501A task.

---
+++ 00601PP0501A.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int nwd(int a, int b) {
+    int tmp;
+    while (b != 0) {
+        tmp = b;
+        b = a % b;
+        a = tmp;
+    }
+    return a;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << nwd(a, b) << endl;
+    }
+}
