ae3440fe4d3c541e26d3e780e4319e957dfc98ce
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-17 22:49:07 +0100

    Added 00997CALC task.

---
+++ 00997CALC.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	char z;
+	int a, b;
+	while (cin >> z >> a >> b) {
+		switch (z)
+		{
+			case '+':
+				cout << a + b << endl;
+				break;
+			case '-':
+				cout << a - b << endl;
+				break;
+			case '*':
+				cout << a * b << endl;
+				break;
+			case '/':
+				cout << a / b << endl;
+				break;
+			case '%':
+				cout << a % b << endl;
+				break;
+		}
+	}
+}
