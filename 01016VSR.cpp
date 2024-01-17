1d50dc4df68ef60ecc8f0f48abe3920927e2133e
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-17 14:12:33 +0100

    Added 01016VSR task.

---
+++ 01016VSR.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCount;
+    cin >> testCount;
+    for (int i = 0; i < testCount; i++) {
+        int speedAtoB, speedBtoA;
+        cin >> speedAtoB >> speedBtoA;
+        int averageSpeed = 2 * speedAtoB * speedBtoA / (speedAtoB + speedBtoA);
+        cout << averageSpeed << endl;
+    }
+    return 0;
+}
