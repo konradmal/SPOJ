a78ef268c8a077e4206345041edfae12b7ce58f2
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-21 22:58:54 +0100

    Added 00506FLAMASTE task.

---
+++ 00506FLAMASTE.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int testCases;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        string text;
+        cin >> text;
+        int count = 1;
+        for (int j = 1; j <= text.length(); j++) {
+            if (j < text.length() && text[j] == text[j - 1]) {
+                count++;
+            } else {
+                if (count > 2) {
+                    cout << text[j - 1] << count;
+                } else if (count == 2) {
+                    cout << text[j - 1] << text[j - 1];
+                } else {
+                    cout << text[j - 1];
+                }
+                count = 1;
+            }
+        }
+        cout << endl;
+    }
+}
