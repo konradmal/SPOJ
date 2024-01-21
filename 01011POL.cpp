f67142ec378a16fdb618f80a54b43bbc823f99f4
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-21 12:57:52 +0100

    Added 01011POL task.

---
+++ 01011POL.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int numberOfTests;
+    cin >> numberOfTests;
+    string text;
+    for (int i = 0; i < numberOfTests; i++) {
+        cin >> text;
+        cout << text.substr(0, text.length() / 2) << endl;
+    }
+}
