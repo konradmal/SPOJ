b88d0ff2daf59569f69d785bff7464af274aa50d
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-21 12:53:09 +0100

    Added 00723PTROL file.

---
+++ 00723PTROL.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCases, arraySize, value, firstValue;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        cin >> arraySize;
+        cin >> firstValue;
+        for (int j = 0; j < arraySize - 1; j++) {
+            cin >> value;
+            cout << value << " ";
+        }
+        cout << firstValue << endl;
+    }
+}
