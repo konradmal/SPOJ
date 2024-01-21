41e198ccd6d15befd1e876398aa93d54c3fc0c9d
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-21 12:22:52 +0100

    Added 00626GLUTTON task.

---
+++ 00626GLUTTON.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    const int secondsInDay = 24 * 60 * 60;
+    int testCases, numContestants, cookiesPerBox, eatingTime;
+    int totalCookiesNeeded = 0;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        cin >> numContestants >> cookiesPerBox;
+        for (int j = 0; j < numContestants; j++) {
+            cin >> eatingTime;
+            totalCookiesNeeded += secondsInDay / eatingTime;
+        }
+        double boxesNeeded = totalCookiesNeeded / static_cast<double>(cookiesPerBox);
+        totalCookiesNeeded = 0;
+        cout << ceil(boxesNeeded) << endl;
+    }
+}
