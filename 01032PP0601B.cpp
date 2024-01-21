43406f0f6919391504d49dc3afef16b016fad531
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-21 21:06:28 +0100

    Added 01032PP0601B task.

---
+++ 01032PP0601B.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCases, upperLimit, divisibleBy, notDivisibleBy;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        cin >> upperLimit >> divisibleBy >> notDivisibleBy;
+        for (int number = divisibleBy; number < upperLimit; number += divisibleBy) {
+            if (number % notDivisibleBy != 0) {
+                cout << number << " ";
+            }
+        }
+        cout << endl;
+    }
+}
