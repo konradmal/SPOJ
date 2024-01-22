e5081283b0abaeb9d134198a62f5326f2105928d
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-22 16:26:57 +0100

    Added 00609ETI06F1 task.

---
+++ 00609ETI06F1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    const double pi = 3.141592654;
+    double radius, diameter, area;
+    cin >> radius >> diameter;
+    double halfDiameterSquared = (diameter / 2) * (diameter / 2);
+    area = pi * (radius * radius - halfDiameterSquared);
+    cout << fixed << setprecision(2) << area << endl;
+}
