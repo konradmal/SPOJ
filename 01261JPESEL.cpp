26df9e920efcb0d4ebc6415705b2d71d4078240a
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-22 10:41:01 +0100

    Added 01261JPESEL task.

---
+++ 01261JPESEL.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCases, sum;
+    cin >> testCases;
+    string pesel;
+    for (int i = 0; i < testCases; i++) {
+        cin >> pesel;
+        sum = (pesel[0] - '0') * 1 + (pesel[1] - '0') * 3 + (pesel[2] - '0') * 7 + 
+                  (pesel[3] - '0') * 9 + (pesel[4] - '0') * 1 + (pesel[5] - '0') * 3 + 
+                  (pesel[6] - '0') * 7 + (pesel[7] - '0') * 9 + (pesel[8] - '0') * 1 + 
+                  (pesel[9] - '0') * 3 + (pesel[10] - '0') * 1;
+        if (sum % 10 == 0)
+            cout << "D" << endl;
+        else
+            cout << "N" << endl;
+    }
+}
