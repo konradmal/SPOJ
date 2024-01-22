378c3f4d4ca26be05431aa8988f61e29702f97bd
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-22 16:33:41 +0100

    Added 00804EUCGAME task.

---
+++ 00804EUCGAME.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    int tmp;
+    while (b != 0) {
+        tmp = b;
+        b = a % b;
+        a = tmp;
+    }
+    return a;
+}
+
+int main() {
+    int testCases, numberA, numberB;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        cin >> numberA >> numberB;
+        cout << 2 * gcd(numberA, numberB) << endl;
+    }
+}
