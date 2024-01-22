148660eb2e9ebc8bd745507df9d48b8a69d6856c
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-22 10:33:22 +0100

    Added 01055PP0602A task.

---
+++ 01055PP0602A.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCases, arraySize, value;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        cin >> arraySize;
+        int* array = new int[arraySize];
+        for (int j = 0; j < arraySize / 2; j++) {
+            cin >> value;
+            array[j + arraySize / 2] = value;
+            cin >> value;
+            array[j] = value;
+        }
+        if (arraySize % 2 == 1) {
+            cin >> value;
+            array[arraySize - 1] = value;
+        }
+
+        for (int j = 0; j < arraySize; j++) {
+            cout << array[j] << " ";
+        }
+        cout << endl;
+        delete[] array;
+    }
+}
