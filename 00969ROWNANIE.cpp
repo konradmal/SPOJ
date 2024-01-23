7604e73771bd969a67444f230db52a2946b2f50e
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-23 16:00:51 +0100

    Added 00969ROWNANIE task.

---
+++ 00969ROWNANIE.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double coeffA, coeffB, coeffC, discriminant;
+    while (cin >> coeffA >> coeffB >> coeffC) {
+        discriminant = coeffB * coeffB - 4 * coeffA * coeffC;
+        if (discriminant > 0)
+            cout << 2 << endl;
+        else if (discriminant < 0)
+            cout << 0 << endl;
+        else
+            cout << 1 << endl;
+    }
+}
