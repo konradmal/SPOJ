9f71fef200a8cb0f50fabed67925cb23b2ce2d20
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-23 16:10:40 +0100

    Added 00708PTCLTZ task.

---
+++ 00708PTCLTZ.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCases, value, stepsCount;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        stepsCount = 0;
+        cin >> value;
+        while (value != 1) {
+            if (value % 2 == 0)
+                value /= 2;
+            else
+                value = 3 * value + 1;
+            stepsCount++;
+        }
+        cout << stepsCount << endl;
+    }
+}
