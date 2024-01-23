47f5fcdb080d9179b0d59be662db2d4d8949ba81
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-23 15:54:49 +0100

    Added 00977TABLICA task.

---
+++ 00977TABLICA.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int number;
+    vector<int> numbers;
+    while (cin >> number) {
+        numbers.push_back(number);
+    }
+    if (!numbers.empty()) {
+        for (int i = numbers.size() - 1; i >= 0; i--) {
+            cout << numbers[i] << " ";
+        }
+    }
+}
