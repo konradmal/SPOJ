71ff831883ab4c61ebf414b628ef8c680dd89cde
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-24 12:20:50 +0100

    Added 01262PP0602D task.

---
+++ 01262PP0602D.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int arraySize, shift;
+    cin >> arraySize >> shift;
+    int* array = new int[arraySize];
+    for (int i = 0; i < arraySize; i++) {
+        cin >> array[i];
+    }
+    for (int i = shift; i < arraySize; i++) {
+        cout << array[i] << " ";
+    }
+    for (int i = 0; i < shift; i++) {
+        cout << array[i] << " ";
+    }
+    delete[] array;
+}
