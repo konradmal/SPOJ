3cbd5030d6b9407f15b590b8b3f219d04af618d5
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-24 12:13:39 +0100

    Added 01228JROWLIN task.

---
+++ 01228JROWLIN.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double a, b, c;
+	cin >> a >> b >> c;
+	if (a == 0 && b == c)
+		cout << "NWR";
+	else if (a == 0 && b != c)
+		cout << "BR";
+	else
+		cout << fixed << setprecision(2) << (c - b) / a;
+}
