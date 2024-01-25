d0f67fe6a933d85a1410707ffb750fbe3522bec3
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-25 18:22:06 +0100

    Added 00998CALC2 task.

---
+++ 00998CALC2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int memory[10] = { 0 };
+    char operation;
+    int operand1, operand2;
+    while (cin >> operation >> operand1 >> operand2) {
+        switch (operation) {
+            case 'z':
+                memory[operand1] = operand2;
+                break;
+            case '+':
+                cout << memory[operand1] + memory[operand2] << endl;
+                break;
+            case '-':
+                cout << memory[operand1] - memory[operand2] << endl;
+                break;
+            case '*':
+                cout << memory[operand1] * memory[operand2] << endl;
+                break;
+            case '/':
+                cout << memory[operand1] / memory[operand2] << endl;
+                break;
+            case '%':
+                cout << memory[operand1] % memory[operand2] << endl;
+                break;
+        }
+    }
+}
