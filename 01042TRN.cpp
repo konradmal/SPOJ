36f02db9dc000d0a0b6398443e20b0fbb18ac922
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-25 18:10:17 +0100

    Added 01042TRN task.

---
+++ 01042TRN.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int rows, columns;
+    cin >> rows >> columns;
+    vector<int> matrix(rows * columns);
+    for (int i = 0; i < rows * columns; i++) {
+        cin >> matrix[i];
+    }
+    for (int i = 0; i < columns; i++) {
+        for (int j = i; j < rows * columns; j += columns) {
+            cout << matrix[j] << " ";
+        }
+        cout << endl;
+    }
+}
