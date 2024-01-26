335e55c82ab8f7d19c4dfea65a24e76c30cfec3b
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-26 12:07:23 +0100

    Added 00617PP0504B task.

---
+++ 00617PP0504B.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCases, minLength;
+    string string1, string2;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        cin >> string1 >> string2;
+        minLength = min(string1.length(), string2.length());
+        for (int j = 0; j < minLength; j++) {
+            cout << string1[j] << string2[j];
+        }
+        cout << endl;
+    }
+}
