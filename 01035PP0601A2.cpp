cac874b222aabd716b94b3531a3326e3084a1ef3
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-26 11:50:44 +0100

    Added 01035PP0601A2 task.

---
+++ 01035PP0601A2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int currentNumber, previousNumber = 42, count42 = 0;
+    while (cin >> currentNumber) {
+        cout << currentNumber << endl;
+        if (currentNumber == 42 && previousNumber != 42)
+            count42++;
+        previousNumber = currentNumber;
+        if (count42 == 3)
+            break;
+    }
+}
