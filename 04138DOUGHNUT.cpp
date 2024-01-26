4c088debf8de77d9dcbf360a6cb1f4b023458ae1
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-26 12:00:44 +0100

    Added 04138DOUGHNUT task.

---
+++ 04138DOUGHNUT.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCases, itemsCount, capacity, itemWeight;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        cin >> itemsCount >> capacity >> itemWeight;
+        if (itemsCount * itemWeight <= capacity)
+            cout << "yes" << endl;
+        else
+            cout << "no" << endl;
+    }
+}
