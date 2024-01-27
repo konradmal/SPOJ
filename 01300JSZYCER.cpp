2775bbdabb69c9430ab9011dd75cc346778939d5
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-27 11:56:19 +0100

    Added 01300JSZYCER task.

---
+++ 01300JSZYCER.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string CaesarCipher(string text, int key) {
+    string result = "";
+    for (int i = 0; i < text.length(); i++) {
+        if (text[i] >= 'A' && text[i] <= 'Z') {
+            char encryptedChar = (text[i] + key - 65) % 26 + 65;
+            if (encryptedChar < 65)
+                encryptedChar += 26;
+            result += encryptedChar;
+        } else {
+            result += text[i];
+        }
+    }
+    return result;
+}
+
+int main() {
+    string s;
+    while (getline(cin, s)) {
+        cout << CaesarCipher(s, 3) << endl;
+    }
+}
