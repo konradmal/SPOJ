759f1803348107e74b0a5142d610edd08721b154
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-27 12:01:01 +0100

    Added 01830KC003 task.

---
+++ 01830KC003.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double sideA, sideB, sideC;
+    while (cin >> sideA >> sideB >> sideC) {
+        if (sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA)
+            cout << 1 << endl;
+        else
+            cout << 0 << endl;
+    }
+}
