25ba11cade4bcb3357e1c5dc96f68bf7619e7b9e
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-28 10:03:59 +0100

    Added 00978STOS task.

---
+++ 00978STOS.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> stack;
+    char command;
+    int number;
+    while (cin >> command) {
+        if (command == '+') {
+            cin >> number;
+            if (stack.size() == 10)
+                cout << ":(" << endl;
+            else {
+                stack.push_back(number);
+                cout << ":)" << endl;
+            }
+        }
+        else {
+            if (stack.size() > 0) {
+                cout << stack[stack.size() - 1] << endl;
+                stack.pop_back();
+            }
+            else
+                cout << ":(" << endl;
+        }
+    }
+}
