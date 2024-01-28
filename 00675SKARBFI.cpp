c7d49f3535f4528f997aa4240016642dd1aed233
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-28 19:09:11 +0100

    Added 00675SKARBFI task.

---
+++ 00675SKARBFI.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCases, numberOfDirections, direction, distance, northSouth = 0, eastWest = 0;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        cin >> numberOfDirections;
+        northSouth = 0;
+        eastWest = 0;
+        for (int j = 0; j < numberOfDirections; j++) {
+            cin >> direction >> distance;
+            switch (direction) {
+                case 0: northSouth += distance; break;
+                case 1: northSouth -= distance; break;
+                case 2: eastWest += distance; break;
+                case 3: eastWest -= distance; break;
+            }
+        }
+        if (northSouth == 0 && eastWest == 0)
+            cout << "studnia" << endl;
+        else {
+            if (northSouth > 0) cout << "0 " << northSouth << endl;
+            else if (northSouth < 0) cout << "1 " << -northSouth << endl;
+
+            if (eastWest > 0) cout << "2 " << eastWest << endl;
+            else if (eastWest < 0) cout << "3 " << -eastWest << endl;
+        }
+    }
+}
