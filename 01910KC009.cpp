3bcd0ca5ae1fa0ba893ae129d83dbd6d328f1c4a
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-28 09:55:09 +0100

    Added 01910KC009 task.

---
+++ 01910KC009.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string text;
+    while (cin >> text) {
+        reverse(text.begin(), text.end());
+        cout << text << endl;
+    }
+}
