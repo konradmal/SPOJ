aaa7d1b7aaede5348c947751daa94cb84f2708f9
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-29 13:58:28 +0100

    Added 01240JWSPLIN task.

---
+++ 01240JWSPLIN.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCases, x1, y1, x2, y2, x3, y3;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+        if ((x2 - x1) * (y3 - y2) == (x3 - x2) * (y2 - y1)) {
+            cout << "TAK" << endl;
+        } else {
+            cout << "NIE" << endl;
+        }
+    }
+}
