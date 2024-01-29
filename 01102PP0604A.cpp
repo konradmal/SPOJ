58951a4981febbc962e541a3a0b62f4cb375ea85
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-29 11:25:32 +0100

    Added 01102PP0604A task.

---
+++ 01102PP0604A.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <cfloat>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int testCases, numberOfValues, value, closestValue;
+    double average, minDifference;
+    vector<int> values;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        cin >> numberOfValues;
+        average = 0;
+        minDifference = DBL_MAX;
+        values.clear();
+        for (int j = 0; j < numberOfValues; j++) {
+            cin >> value;
+            values.push_back(value);
+            average += value;
+        }
+        average /= values.size();
+        for (int j = 0; j < numberOfValues; j++) {
+            double currentDifference = abs(values[j] - average);
+            if (currentDifference < minDifference) {
+                minDifference = currentDifference;
+                closestValue = values[j];
+            }
+        }
+        cout << closestValue << endl;
+    }
+}
