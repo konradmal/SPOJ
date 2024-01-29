df6768d7d617510df6b19281229ac704c53d2d7d
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-29 11:38:20 +0100

    Added 01242JZLICZ task.

---
+++ 01242JZLICZ.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    int lowerCaseCount[26] = { 0 };
+    int upperCaseCount[26] = { 0 };
+    string text;
+    cin.ignore();
+    for (int i = 0; i < t; i++) {
+        getline(cin, text);
+        for (char ch : text) {
+            if (ch >= 'a' && ch <= 'z')
+                lowerCaseCount[ch - 'a']++;
+            else if (ch >= 'A' && ch <= 'Z')
+                upperCaseCount[ch - 'A']++;
+        }
+    }
+    for (int i = 0; i < 26; i++) {
+        if (lowerCaseCount[i] > 0)
+            cout << char(i + 'a') << " " << lowerCaseCount[i] << endl;
+    }
+    for (int i = 0; i < 26; i++) {
+        if (upperCaseCount[i] > 0)
+            cout << char(i + 'A') << " " << upperCaseCount[i] << endl;
+    }
+}
