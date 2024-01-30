d94266b590203beb19f9077a355447f453af2824
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-30 12:01:04 +0100

    Added 01276JSPACE task.

---
+++ 01276JSPACE.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string inputLine, outputLine;
+    bool capitalizeNext;
+    while (getline(cin, inputLine)) {
+        outputLine = "";
+        for (int i = 0; i < inputLine.length(); i++) {
+            capitalizeNext = false;
+            while (isspace(inputLine[i])) {
+                i++;
+                capitalizeNext = true;
+            }
+            if (capitalizeNext) {
+                outputLine += toupper(inputLine[i]);
+            }
+            else {
+                outputLine += inputLine[i];
+            }
+        }
+        cout << outputLine << endl;
+    }
+}
