f568bdd6750d49228c3f2d51caf536bc09da9fb7
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-30 11:34:48 +0100

    Added 01289JHTMLLET task.

---
+++ 01289JHTMLLET.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string inputLine, outputLine;
+    while (getline(cin, inputLine)) {
+        outputLine = "";
+        bool insideTag = false;
+        for (char ch : inputLine) {
+            if (ch == '<') {
+                insideTag = true;
+            }
+            if (insideTag) {
+                outputLine += toupper(ch);
+            } else {
+                outputLine += ch;
+            }
+            if (ch == '>') {
+                insideTag = false;
+            }
+        }
+        cout << outputLine << endl;
+    }
+}
