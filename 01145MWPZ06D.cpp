728fbda7125e917bf6cc056a1d8aba5c5cb0ee38
konradmal <42766275+konradmal@users.noreply.github.com>
2024-01-31 15:38:45 +0100

    Added 01145MWPZ06D task.

---
+++ 01145MWPZ06D.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCases, classSize, candies;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        cin >> classSize >> candies;
+        if (classSize == 0 || candies == 0)
+            cout << "NIE" << endl;
+        else if (classSize == 1)
+            cout << "TAK" << endl;
+        else if (candies % (classSize - 1) == 0)
+            cout << "NIE" << endl;
+        else
+            cout << "TAK" << endl;
+    }
+}
