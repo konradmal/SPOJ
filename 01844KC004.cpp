a5d771a887c8e23a17087e2650745ddbe8dcc7ce
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-01 19:06:39 +0100

    Added 01844KC004 task.

---
+++ 01844KC004.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main() {
+    char z;
+    int value, numberToFind = -1, numberOfTests = -1, count = 0;
+    while (cin.get(z)) {
+        if (isdigit(z)) {
+            cin.unget();
+            cin >> value;
+            if (numberToFind == -1)
+                numberToFind = value;
+            else if (numberOfTests == -1)
+                numberOfTests = value;
+            else if (value == numberToFind)
+                count++;
+        }
+        else if (z == '\n') {
+            cout << count << endl;
+            numberToFind = -1;
+            numberOfTests = -1;
+            count = 0;
+        }
+    }
+}
