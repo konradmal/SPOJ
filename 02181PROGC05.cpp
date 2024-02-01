4f458444b81979011b7a9ffcc870a036fe0fc2a7
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-01 21:37:55 +0100

    Added 02181PROGC05 task.

---
+++ 02181PROGC05.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    char excludedChar;
+    string inputString;
+    while (cin >> excludedChar) {
+        cin >> inputString;
+        for (int i = 0; i < inputString.length(); i++) {
+            if (inputString[i] != excludedChar)
+                cout << inputString[i];
+        }
+        cout << endl;
+    }
+}
