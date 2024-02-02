3626b9188eda10c1125c5a8af46aa167603b7aea
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-02 09:28:12 +0100

    Added 01596WSEGA task.

---
+++ 01596WSEGA.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCases, segments, legs, totalLegs;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        totalLegs = 0;
+        cin >> segments;
+        for (int j = 0; j < segments; j++) {
+            cin >> legs;
+            totalLegs += legs;
+        }
+        cout << totalLegs + segments - 1 << endl;
+    }
+}
