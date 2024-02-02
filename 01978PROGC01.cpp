1b4a85705b4a59e8f592ccb1ce06e7c166ac96de
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-02 13:23:02 +0100

    Added 01978PROGC01 task.

---
+++ 01978PROGC01.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int lineCount = 0;
+    string line;
+    while (getline(cin, line))
+        lineCount++;
+    cout << lineCount << endl;
+}
