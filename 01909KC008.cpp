d33ba9fa75efad68f43f155a1e7dc45b3abc0d64
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-02 09:12:51 +0100

    Added 01909KC008 task.

---
+++ 01909KC008.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    long int number, partialSum = 0, totalSum = 0;
+    while (cin >> number) {
+        if (number == 0) {
+            cout << partialSum << endl;
+            totalSum += partialSum;
+            partialSum = 0;
+        } else {
+            partialSum += number;
+        }
+    }
+    cout << totalSum << endl;
+}
