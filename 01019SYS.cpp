ed2b7a54ff7d3517d324a31b25398b3a0d2dc637
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-04 11:26:48 +0100

    Added 01019SYS task.

---
+++ 01019SYS.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+string ConvertToBase(int number, int base) {
+    string result = "";
+    while (number > 0) {
+        int remainder = number % base;
+        if (remainder <= 9) {
+            result = char(remainder + '0') + result;
+        }
+        else {
+            result = char(remainder - 10 + 'A') + result;
+        }
+        number /= base;
+    }
+    return result == "" ? "0" : result;
+}
+
+int main() {
+    int testCases, number;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        cin >> number;
+        cout << ConvertToBase(number, 16) << " " << ConvertToBase(number, 11) << endl;
+    }
+}
