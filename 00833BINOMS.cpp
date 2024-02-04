ef4207e8eb06dd8d3c470889142fc7ed4e872f65
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-04 11:36:48 +0100

    Added 00833BINOMS task.

---
+++ 00833BINOMS.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+long long binomialCoefficient(int n, int k) {
+    if (k > n - k)
+        k = n - k;
+    long long result = 1;
+    for (int i = 0; i < k; ++i) {
+        result *= (n - i);
+        result /= (i + 1);
+    }
+    return result;
+}
+
+int main() {
+    int testCases, a, b;
+    cin >> testCases;
+    for (int i = 0; i < testCases; ++i) {
+        cin >> a >> b;
+        cout << binomialCoefficient(a, b) << endl;
+    }
+}
