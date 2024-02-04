9a4ee2bfa97f67c51b2a9443fdeecba9f5c73446
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-04 12:59:27 +0100

    Added 01299FZI_STEF task.

---
+++ 01299FZI_STEF.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    long long numberOfCities, currentProfit = 0, maxProfit = 0, profit;
+    cin >> numberOfCities;
+    for (int i = 0; i < numberOfCities; i++) {
+        cin >> profit;
+        currentProfit += profit;
+        if (currentProfit > maxProfit) {
+            maxProfit = currentProfit;
+        } 
+        if (currentProfit < 0) {
+            currentProfit = 0;
+        }
+    }
+    cout << maxProfit << endl;
+}
