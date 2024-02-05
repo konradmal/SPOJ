2f64d12c31d843842231d2d62fbba55060d2d78f
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-05 20:04:00 +0100

    Added 01211NIEKOLEJ task.

---
+++ 01211NIEKOLEJ.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int totalNumbers;
+    cin >> totalNumbers;
+    if (totalNumbers == 0)
+        cout << 0;
+    if (totalNumbers <= 2)
+        cout << "NIE";
+    else {
+        for (int odd = 1; odd <= totalNumbers; odd += 2)
+            cout << odd << " ";
+        for (int even = 0; even <= totalNumbers; even += 2)
+            cout << even << " ";
+    }
+}
