a18d24082679aebd42987b293074571f8886466a
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-05 20:11:50 +0100

    Added 03326PASTAB2 task.

---
+++ 03326PASTAB2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int totalNumbers;
+    long number, comparisonValue;
+    vector<long> numbers;
+    char operation;
+    
+    cin >> totalNumbers;
+    for (int i = 0; i < totalNumbers; i++) {
+        cin >> number;
+        numbers.push_back(number);
+    }
+    
+    cin >> operation >> comparisonValue;
+    for (int i = 0; i < numbers.size(); i++) {
+        if ((operation == '>' && numbers[i] > comparisonValue) || 
+            (operation == '<' && numbers[i] < comparisonValue)) {
+            cout << numbers[i] << endl;
+        }
+    }
+}
