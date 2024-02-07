d418fa36f23f4bb7cdd5830a2a518dc997fabb8f
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-07 12:49:12 +0100

    Added 01911KC010 task.

---
+++ 01911KC010.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+    int wordsCount, numbersCount, index;
+    string textLine;
+    while (getline(cin, textLine)) {
+        wordsCount = 0;
+        numbersCount = 0;
+        index = 0;
+        while (index < textLine.length()) {
+            if (isdigit(textLine[index]))
+                numbersCount++;
+            else
+                wordsCount++;
+            while (index < textLine.length() && textLine[index] != ' ')
+                index++;
+            index++;
+        }
+        cout << numbersCount << " " << wordsCount << endl;
+    }
+}
