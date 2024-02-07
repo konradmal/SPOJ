b3f4b7dcc8351bfc7a67e0ce8f7f725a3306aebe
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-07 13:23:39 +0100

    Added 01142MWPZ06A task.

---
+++ 01142MWPZ06A.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int numberOfCases;
+    double ageDifference, timesYounger, currentAge, calculationResult;
+    cin >> numberOfCases;
+    for (int i = 0; i < numberOfCases; i++) {
+        cin >> ageDifference >> timesYounger >> currentAge;
+        calculationResult = (timesYounger * currentAge - (ageDifference + timesYounger)) / (currentAge - 1);
+        cout << round(calculationResult * 12) << endl;
+    }
+}
