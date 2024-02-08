e0ca73383af4120be16e34a68ce3d9cc91660080
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-08 05:06:41 +0100

    Added 27236AL_28_01 task.

---
+++ 27236AL_28_01.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int length;
+    string word;
+    cin >> length;
+    cin >> word;
+    for (int layer = length / 2; layer >= 0; layer--) {
+        for (int dotLeft = 0; dotLeft < layer; dotLeft++)
+            cout << ".";
+        for (int letterIndex = layer; letterIndex < length - layer; letterIndex++)
+            cout << word[letterIndex];
+        for (int dotRight = 0; dotRight < layer; dotRight++)
+            cout << ".";
+        cout << endl;
+    }
+}
