bbe0de1066abf324abbb09bfee13df67f0931b02
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-08 04:30:13 +0100

    Added 05517PICIRC task.

---
+++ 05517PICIRC.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double centerX, centerY, radius, pointX, pointY, distance;
+    cin >> centerX >> centerY >> radius;
+    int numberOfPoints;
+    cin >> numberOfPoints;
+    for (int i = 0; i < numberOfPoints; i++) {
+        cin >> pointX >> pointY;
+        distance = sqrt((centerX - pointX) * (centerX - pointX) + (centerY - pointY) * (centerY - pointY));
+        if (distance < radius)
+            cout << "I" << endl;
+        else if (distance == radius)
+            cout << "E" << endl;
+        else
+            cout << "O" << endl;
+    }
+}
