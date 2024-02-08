8916e89ff6eb09ed1d27498adb05ba8318ffae24
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-08 04:47:16 +0100

    Added 04799WI_TRIGR task.

---
+++ 04799WI_TRIGR.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        for (int i = 0; i < line.length(); i++) {
+            if (i + 2 < line.length() && line[i] == '?' && line[i + 1] == '?') {
+                switch (line[i + 2]) {
+                    case '=': cout << '#'; i += 2; break;
+                    case '/': cout << '\\'; i += 2; break;
+                    case '\'': cout << '^'; i += 2; break;
+                    case '(': cout << '['; i += 2; break;
+                    case ')': cout << ']'; i += 2; break;
+                    case '!': cout << '|'; i += 2; break;
+                    case '<': cout << '{'; i += 2; break;
+                    case '>': cout << '}'; i += 2; break;
+                    case '-': cout << '~'; i += 2; break;
+                    default: cout << line[i]; break;
+                }
+            } else {
+                cout << line[i];
+            }
+        }
+        cout << endl;
+    }
+}
