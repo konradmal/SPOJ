ee53b3c1fc3b3a2c1c9604e6eb4fb1e26a2cf6c0
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-09 20:29:40 +0100

    Added 35257FR_10_02 task.

---
+++ 35257FR_10_02.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int letterCount;
+    cin >> letterCount;
+    vector<char> doubledLetters(letterCount);
+    string word, result;
+    for (int i = 0; i < letterCount; i++) {
+        cin >> doubledLetters[i];
+    }
+    cin >> word;
+    for (char currentChar : word) {
+        result += currentChar;
+        for (char doubledChar : doubledLetters) {
+            if (currentChar == doubledChar) {
+                result += currentChar;
+                break;
+            }
+        }
+    }
+    cout << result;
+}
