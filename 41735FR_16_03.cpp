27898cddf199778874303a33ba758cf6f02dc4fe
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-09 16:30:26 +0100

    Added 41735FR_16_03 task.

---
+++ 41735FR_16_03.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string snakeCaseVariable;
+    cin >> snakeCaseVariable;
+    for (int i = 0; i < snakeCaseVariable.length(); i++) {
+        if (isalpha(snakeCaseVariable[i]))
+            cout << snakeCaseVariable[i];
+        else {
+            i++;
+            if (i < snakeCaseVariable.length()) {
+                cout << char(toupper(snakeCaseVariable[i]));
+            }
+        }
+    }
+}
