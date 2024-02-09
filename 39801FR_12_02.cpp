03173c681dcd099cf93e29ff642de7a04ef88f95
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-09 20:20:24 +0100

    Added 39801FR_12_02 task.

---
+++ 39801FR_12_02.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    string tower1Position, tower2Position;
+    cin >> tower1Position >> tower2Position;
+    if (tower1Position[0] == tower2Position[0] || tower1Position[1] == tower2Position[1])
+        cout << "TAK";
+    else
+        cout << "NIE";
+}
