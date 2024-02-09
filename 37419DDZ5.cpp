b78e6c3df12aa6c57bc36345edf081a70c733f5d
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-09 20:51:02 +0100

    Added 37419DDZ5 task.

---
+++ 37419DDZ5.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int sequenceLength, number, tripletsCount = 0;
+    cin >> sequenceLength;
+    vector<int> sequence;
+    for (int i = 0; i < sequenceLength; i++) {
+        cin >> number;
+        sequence.push_back(number);
+    }
+    for (int i = 2; i < sequenceLength; i++) {
+        if (sequence[i] == sequence[i - 1] + sequence[i - 2]) {
+            tripletsCount++;
+        }
+    }
+    cout << tripletsCount;
+}
