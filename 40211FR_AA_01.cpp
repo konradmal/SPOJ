7b8ff5c58c89e696edc8fd44ed026dfa07790d34
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-09 16:21:54 +0100

    Added 40211FR_AA_01 task.

---
+++ 40211FR_AA_01.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int totalStationaryParticipants = 0, totalParticipants, remoteParticipants;
+    for (int i = 0; i < 4; i++) {
+        cin >> totalParticipants >> remoteParticipants;
+        totalStationaryParticipants += totalParticipants - remoteParticipants;
+    }
+    cout << totalStationaryParticipants;
+}
