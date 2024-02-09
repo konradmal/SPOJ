d10464850381e8344f16620fbb90af235b60443c
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-09 20:43:27 +0100

    Added 37488WWO_01_05 task.

---
+++ 37488WWO_01_05.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip> // Dla std::setfill i std::setw
+using namespace std;
+
+int toDecimal(const string& binaryNumber) {
+    int result = 0;
+    for (int i = 0; i < binaryNumber.length(); i++) {
+        if (binaryNumber[i] == '1') {
+            result += static_cast<int>(pow(2, binaryNumber.length() - i - 1));
+        }
+    }
+    return result;
+}
+
+int main() {
+    string binaryHourTens, binaryHourOnes, binaryMinuteTens, binaryMinuteOnes;
+    cin >> binaryHourTens >> binaryHourOnes >> binaryMinuteTens >> binaryMinuteOnes;
+    cout << toDecimal(binaryHourTens) << toDecimal(binaryHourOnes) << ":" << toDecimal(binaryMinuteTens) << toDecimal(binaryMinuteOnes);
+}
