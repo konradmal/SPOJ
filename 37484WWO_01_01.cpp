d5cfc70d6573e36f03c98f63093bfa85c7df7604
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-10 11:43:44 +0100

    Added 37484WWO_01_01 task.

---
+++ 37484WWO_01_01.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int class3a, class3b, class4j, boys = 0, girls = 0;
+    cin >> class3a >> class3b >> class4j;
+    string name;
+    for (int i = 0; i < class3a + class3b + class4j; i++) {
+        cin >> name;
+        if (name[name.length() - 1] == 'a')
+            girls++;
+        else
+            boys++;
+    }
+    cout << min(girls, boys);
+}
