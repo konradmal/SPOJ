d0c83eab3da3559a179d39632b06ece1a5aec59b
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-10 11:12:12 +0100

    Added 04629PTWPZ083 task.

---
+++ 04629PTWPZ083.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCases;
+    cin >> testCases;
+    string expression;
+    for (int i = 0; i < testCases; i++) {
+        cin >> expression;
+        int result = expression[0] - '0';
+        for (size_t j = 1; j < expression.length(); j += 2) {
+            char operation = expression[j];
+            int nextDigit = expression[j + 1] - '0';
+            if (operation == '+') {
+                result += nextDigit;
+            } else {
+                result -= nextDigit;
+            }
+        }
+        cout << result << endl;
+    }
+}
