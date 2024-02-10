d602afd3a377c3ef1a21ff4c860efda193564d71
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-10 11:52:32 +0100

    Added 34475FR_09_06 task.

---
+++ 34475FR_09_06.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int maxi, mini, testCases;
+	string word;
+	cin >> testCases;
+	for (int i = 0; i < testCases; i++) {
+		int maxChar = 'A';
+		int miniChar = 'Z';
+		cin >> word;
+		for (int j = 0; j < word.length(); j++) {
+			maxChar = max(maxChar, int(word[j]));
+			miniChar = min(miniChar, int(word[j]));
+		}
+		cout << maxChar - miniChar << endl;
+	}
+}
