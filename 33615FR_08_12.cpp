d5868fc1ff331aa2c23c7cc05f0107e5a54014ba
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-11 10:26:19 +0100

    Added 33615FR_08_12 task.

---
+++ 33615FR_08_12.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int testCases;
+    long long number;
+    int base;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        cin >> number >> base;
+        if (number == 0)
+            cout << 1 << endl;
+        else
+            cout << static_cast<int>(floor(log(number) / log(base)) + 1) << endl;
+    }
+}
