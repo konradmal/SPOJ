c28970f14204818f87927604ab2b24faa2e7558a
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-11 10:02:04 +0100

    Added 36168ML1_B task.

---
+++ 36168ML1_B.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int currentDepth = 0, maxDepth = 0, linesCount;
+    string instruction;
+    cin >> linesCount;
+
+    for (int i = 0; i < linesCount; i++) {
+        cin >> instruction;
+        if (instruction == "for") {
+            currentDepth++;
+            maxDepth = max(maxDepth, currentDepth);
+        } else if (instruction == "end")
+            currentDepth--;
+    }
+    if (maxDepth == 0)
+        cout << "O(1)";
+    else if (maxDepth == 1)
+        cout << "O(n)";
+    else
+        cout << "O(n^" << maxDepth << ")";
+}
