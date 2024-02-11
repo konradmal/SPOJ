f79bd14d4b5afd37dddadd07055bca1dea3e0398
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-11 10:09:48 +0100

    Added 40212FR_AA_02 task.

---
+++ 40212FR_AA_02.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int favoriteNumber, birthYear;
+    cin >> favoriteNumber >> birthYear;
+    int lastDigit = favoriteNumber % 10;
+    int calculation = lastDigit * 2;
+    calculation = (calculation + 5) * 50 + 1771;
+    calculation -= birthYear;
+    cout << calculation % 100;
+}
