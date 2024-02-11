b934daec9bb8a5aef8316a82968dae81eb0c87ca
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-11 09:52:47 +0100

    Added 41139FR_15_01 task.

---
+++ 41139FR_15_01.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    const double KM_TO_MILES = 1.609344;
+    double kilometers;
+    cin >> kilometers;
+    double miles = kilometers / KM_TO_MILES;
+    if (miles < 300)
+        cout << "NIE";
+    else if (miles < 500)
+        cout << "SPRAWDZIMY TWOJE OBECNE BUTY";
+    else
+        cout << "TAK";
+}
