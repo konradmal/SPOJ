4ad78f09155787e9d2b12a89c9603f01a0d5ad02
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-12 16:28:50 +0100

    Added 24696FR_04_05 task.

---
+++ 24696FR_04_05.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	int testCases;
+	double radius1, radius2, moonAreaSum;
+	cin >> testCases;
+	for (int i = 0; i < testCases; i++) {
+		cin >> radius1 >> radius2;
+		radius1 *= 2;
+		radius2 *= 2;
+		moonAreaSum = radius1 * radius2 / 2;
+		cout << fixed << setprecision(2) << moonAreaSum << endl;
+	}
+}
