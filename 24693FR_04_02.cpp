8927e300c334b3aef14448447b42034e22345dec
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-12 16:39:30 +0100

    Added 24693FR_04_02 task.

---
+++ 24693FR_04_02.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int queries, numPeople, querySeat, oppositeSeat;
+    cin >> queries;
+    
+    for (int i = 0; i < queries; i++) {
+        cin >> numPeople >> querySeat;
+        if (numPeople % 2 == 1)
+            cout << "BRAK" << endl;
+        else {
+            oppositeSeat = querySeat + numPeople / 2;
+            if (oppositeSeat > numPeople)
+                oppositeSeat -= numPeople;
+            cout << oppositeSeat << endl;
+        }
+    }
+}
