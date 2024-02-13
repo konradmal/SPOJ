e645a09424f19bbe363e2c645dde1dacb1ca44bf
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-13 08:11:47 +0100

    Added 39805FR_12_06 task.

---
+++ 39805FR_12_06.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int balance = 0;
+    string grades;
+    cin >> grades;
+    for (char grade : grades) {
+        if (grade == '+')
+            balance++;
+        else
+            balance--;
+    }
+    int netBalance = balance / 3;
+    if (netBalance > 0)
+        for (int i = 0; i < netBalance; i++)
+            cout << 5 << " ";
+    else if (netBalance < 0)
+        for (int i = netBalance; i < 0; i++)
+            cout << 1 << " ";
+    else
+        cout << "BRAK";
+}
