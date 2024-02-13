23f835ef0b8f04010587e3f55864bad6640dc4f0
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-13 08:19:34 +0100

    Added 26537MWP8_1A task.

---
+++ 26537MWP8_1A.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int gcdRecursiveOptimized(int a, int b){
+    if (!b){
+        return a;
+    }
+    return gcdRecursiveOptimized(b, a % b);
+}
+
+int main() {
+    int numberOfElements, element;
+    vector<int> sequenceElements;
+    cin >> numberOfElements;
+    for (int i = 0; i < numberOfElements; i++) {
+        cin >> element;
+        sequenceElements.push_back(element);
+    }
+    for (int i = 0; i < sequenceElements.size(); i++)
+        for (int j = 0; j < sequenceElements.size(); j++)
+            cout << gcdRecursiveOptimized(sequenceElements[i], sequenceElements[j]) << " ";
+        cout << endl;
+}
