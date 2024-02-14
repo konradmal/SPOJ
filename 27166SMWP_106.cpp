350d4c79b429ec9923d004a6843844b2211aa438
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-14 19:17:07 +0100

    Added 27166SMWP_106 task.

---
+++ 27166SMWP_106.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPerfectGraph(int edges) {
+    double V = (1 + sqrt(1 + 8 * edges)) / 2;
+    return int(V) == V;
+}
+
+int main() {
+    int testsCount, connections;
+    cin >> testsCount;
+    for (int i = 0; i < testsCount; i++) {
+        cin >> connections;
+        if (isPerfectGraph(connections)) {
+            cout << "TAK" << endl;
+        } else {
+            cout << "NIE" << endl;
+        }
+    }
+}
