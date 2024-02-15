172f0c54a5bf13d45f11d5d5a5a593dc7d98567d
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-15 19:06:29 +0100

    Added 28690FR_06_05 task.

---
+++ 28690FR_06_05.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int testCases, numberOfPoints;
+    cin >> testCases;
+    double startX, startY, endX, endY, distance;
+    for (int i = 0; i < testCases; i++) {
+        cin >> numberOfPoints;
+        distance = 0;
+        cin >> startX >> startY;
+        startX /= 1000;
+        startY /= 1000;
+        for (int j = 1; j < numberOfPoints; j++) {
+            cin >> endX >> endY;
+            endX /= 1000;
+            endY /= 1000;
+            distance += sqrt(pow(endX - startX, 2) + pow(endY - startY, 2));
+            startX = endX;
+            startY = endY;
+        }
+        cout << setprecision(2) << fixed << distance << endl;
+    }
+}
