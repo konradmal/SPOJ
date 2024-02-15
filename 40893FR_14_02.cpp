84585e18da126e1f7512d6cd3352044ec0f8eeef
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-15 18:53:29 +0100

    Added 40893FR_14_02 task.

---
+++ 40893FR_14_02.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    string word;
+    cin >> word;
+    bool containsSto = false;
+    for (int i = 2; i < word.length(); i++)
+        if (word[i - 2] == 's' && word[i - 1] == 't' && word[i] == 'o') {
+            cout << "TAK";
+            containsSto = true;
+            break;
+        }
+    if (!containsSto)
+        cout << "NIE";
+}
