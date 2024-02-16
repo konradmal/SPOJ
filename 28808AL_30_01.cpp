fe4dffcf584615c9669883c54640e0bdd880a326
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-16 11:07:47 +0100

    Added 28808AL_30_01 task.

---
+++ 28808AL_30_01.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool isAnagram(string word1, string word2) {
+    sort(word1.begin(), word1.end());
+    sort(word2.begin(), word2.end());
+    return word1 == word2;
+}
+
+int main() {
+    string baseWord, testWord;
+    int numOfTests, countAnagrams = 0;
+    cin >> baseWord >> numOfTests;
+    for (int i = 0; i < numOfTests; i++) {
+        cin >> testWord;
+        if (isAnagram(baseWord, testWord))
+            countAnagrams++;
+    }
+    cout << countAnagrams;
+}
