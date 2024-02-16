f4c30a354aff3c589127aebe06a17454b139c203
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-16 11:37:40 +0100

    Added 24695FR_04_04 task.

---
+++ 24695FR_04_04.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCases, charCount;
+    cin >> testCases;
+    string expression;
+    char currentChar, previousChar, previousOutput;
+    for (int i = 0; i < testCases; i++) {
+        cin >> expression;
+        charCount = 0;
+        while (charCount < expression.length() && expression[charCount] == '|') {
+            cout << "(";
+            charCount++;
+        }
+        if (charCount == 0) {
+            cout << expression[0];
+            charCount++;
+        }
+        previousOutput = expression[charCount - 1];
+        for (; charCount < expression.length(); charCount++) {
+            currentChar = expression[charCount];
+            previousChar = expression[charCount - 1];
+            if (currentChar == '|' && (previousOutput == '(' || previousChar == '-' || previousChar == '+')) {
+                cout << "(";
+                previousOutput = '(';
+            }
+            else if (currentChar == '|' && !(previousOutput == '(' || previousChar == '-' || previousChar == '+')) {
+                cout << ")";
+                previousOutput = ')';
+            } else {
+                cout << currentChar;
+                previousOutput = currentChar;
+            }
+        }
+        cout << endl;
+    }
+}
