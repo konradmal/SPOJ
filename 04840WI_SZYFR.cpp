b53b0563bfe17affb1ad49ec60f27fedad1c210c
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-17 10:17:54 +0100

    Added 04840WI_SZYFR task.

---
+++ 04840WI_SZYFR.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+char shiftChar(char ch, int shift) {
+    if (shift < 0)
+        shift += 26;
+    return (ch - 'A' + shift) % 26 + 'A';
+}
+
+int main() {
+    int i = 0;
+    string action, key, text, keyStr;
+    cin >> action >> key >> text;
+    while (key[i] == '0')
+        i++;
+    while (i < key.length()) {
+        keyStr += key[i];
+        i++;
+    }
+    vector<int> keyDigits;
+    for (char ch : keyStr)
+        keyDigits.push_back(ch - '0');
+    for (int i = 0; i < text.length(); ++i) {
+        int shift = keyDigits[i % keyDigits.size()];
+        if (action == "DESZYFRUJ")
+            shift = -shift;
+        cout << shiftChar(text[i], shift);
+    }
+}
