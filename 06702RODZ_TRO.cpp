18166a8725e2b1c504d1d265db00d9cdec2da635
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-17 12:51:53 +0100

    Added 06702RODZ_TRO task.

---
+++ 06702RODZ_TRO.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	while (cin >> a >> b >> c) {
+		if (!(a + b > c && a + c > b && b + c > a))
+			cout << "brak" << endl;
+		else if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a)
+			cout << "prostokatny" << endl;
+		else if (a * a + b * b < c * c || a * a + c * c < b * b || b * b + c * c < a * a)
+			cout << "rozwartokatny" << endl;
+		else
+			cout << "ostrokatny" << endl;
+	}
+}
