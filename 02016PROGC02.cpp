b0c5cad2b757e59e3a091940963b2c36a1520715
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-18 17:57:41 +0100

    Added 02016PROGC02 task.

---
+++ 02016PROGC02.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string inputLine;
+    int asciiCounts[256] = {};
+    while (getline(cin, inputLine)) {
+        asciiCounts[10]++;
+        for (char character : inputLine)
+            asciiCounts[static_cast<unsigned char>(character)]++;
+    }
+    for (int i = 0; i < 256; i++)
+        if (asciiCounts[i] > 0)
+            cout << i << " " << asciiCounts[i] << endl;
+}
