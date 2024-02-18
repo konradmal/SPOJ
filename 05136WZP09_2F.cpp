ceacb5593c79141e4f83fd5b0fcce35157d6a46c
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-18 17:37:19 +0100

    Added 05136WZP09_2F task.

---
+++ 05136WZP09_2F.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    string number;
+    int digitSum;
+    while (cin >> number && number != "0") {
+        digitSum = 0;
+        for (char digit : number)
+            digitSum += digit - '0';
+        if ((number.back() == '0' || number.back() == '5') && digitSum % 3 == 0)
+            cout << "TAK" << endl;
+        else
+            cout << "NIE" << endl;
+    }
+}
