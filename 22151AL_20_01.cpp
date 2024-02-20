aeeb4b9d4cfb2de054632b3d67c2b3826b052277
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-20 10:00:39 +0100

    Added 22151AL_20_01 task.

---
+++ 22151AL_20_01.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string morseCode(char letter) {
+    switch (letter) {
+        case 'A': return ".-";
+        case 'B': return "-...";
+        case 'C': return "-.-.";
+        case 'D': return "-..";
+        case 'E': return ".";
+        case 'F': return ".-..";
+        case 'G': return "--.";
+        case 'H': return "....";
+        case 'I': return "..";
+        case 'J': return ".---";
+        case 'K': return "-.-";
+        case 'L': return "..-.";
+        case 'M': return "--";
+        case 'N': return "-.";
+        case 'O': return "---";
+        case 'P': return ".--.";
+        case 'Q': return "--.-";
+        case 'R': return ".-.";
+        case 'S': return "...";
+        case 'T': return "-";
+        case 'U': return "..-";
+        case 'V': return "...-";
+        case 'W': return ".--";
+        case 'X': return "-..-";
+        case 'Y': return "-.--";
+        case 'Z': return "--..";
+        default: return "";
+    }
+}
+
+int main()
+{
+	string line;
+    char letter;
+    while (getline(cin, line)) {
+	    for (int i = 0; i < line.length(); i++) {
+		    letter = toupper(line[i]);
+            cout << morseCode(letter) << "/";
+	    }
+        cout << endl;
+    }
+}
