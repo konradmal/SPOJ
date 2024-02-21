4a7b9f9d9f3030f7ce6d33899bd37b41aa67bcbe
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-21 11:32:00 +0100

    Added 14779AL_06_01 task.

---
+++ 14779AL_06_01.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int testCases, dividend, divisor, rest;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        cin >> dividend >> divisor;
+        rest = dividend % divisor;
+        if (rest < 0)
+            rest += abs(divisor);
+        cout << rest << endl;
+    }
+}
