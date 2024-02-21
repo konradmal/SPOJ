034c3c706ca7fb2f768577cdf5190f8f4e1f593f
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-21 11:20:40 +0100

    Added 09839KART task.

---
+++ 09839KART.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int findZeros(int n) {
+    int count = 0;
+    for (int i = 5; n / i >= 1; i *= 5) {
+        count += n / i;
+    }
+    return count;
+}
+
+int main() {
+    int testCases, number;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        cin >> number;
+        cout << findZeros(number) << endl;
+    }
+}
