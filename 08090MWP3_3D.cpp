ee40eeef2fb336f020fe6ec2671960e4255fba83
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-22 10:59:57 +0100

    Added 08090MWP3_3D task.

---
+++ 08090MWP3_3D.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+const double pi = acos(-1.0);
+
+int main()
+{
+	int testCases;
+	cin >> testCases;
+	double boxSide, people;
+	for (int i = 0; i < testCases; i++) {
+		cin >> boxSide >> people;
+		if (int(people) % 2 == 0)
+			cout << fixed << setprecision(3) << boxSide * pi / people << " " << int(people) / 2 << endl;
+		else
+			cout << fixed << setprecision(3) << boxSide * pi / people / 2 << " " << int(people) << endl;
+	}
+}
