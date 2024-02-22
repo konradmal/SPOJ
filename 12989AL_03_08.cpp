bda505db544a8c84a3ebb78b207d0163f385720a
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-22 10:43:12 +0100

    Added 12989AL_03_08 task.

---
+++ 12989AL_03_08.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int currentInput = -1, highestValue = 0, chosenPresent = 0;
+    while(currentInput != 0) {
+        if(currentInput > chosenPresent && currentInput != highestValue)
+            chosenPresent = currentInput;
+        cin >> currentInput;
+        if(currentInput > highestValue) {
+            chosenPresent = highestValue;
+            highestValue = currentInput;
+        }
+    }
+    if(chosenPresent == 0)
+        chosenPresent = highestValue;
+    cout << chosenPresent;
+}
