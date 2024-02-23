b1168277cc3b4f999a13bb06dab1c5aa87f7bc46
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-23 14:30:29 +0100

    Added 19278AL_15_01 task.

---
+++ 19278AL_15_01.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    string darekAnswers, jarekAnswers, marekAnswers;
+    cin >> darekAnswers >> jarekAnswers >> marekAnswers;
+    int maxPoints = 0;
+    for (int i = 0; i < darekAnswers.length(); i++) {
+        if (darekAnswers[i] != jarekAnswers[i]) 
+            maxPoints++;
+        if (darekAnswers[i] != marekAnswers[i] && marekAnswers[i] == jarekAnswers[i])
+            maxPoints++;
+        if (jarekAnswers[i] == darekAnswers[i] && marekAnswers[i] != jarekAnswers[i])
+            maxPoints++;
+    }
+    cout << maxPoints;
+}
