af9dd6d9f283f80d044a524da5d85ad3a0a5bfc6
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-25 19:09:25 +0100

    Added 08354BICAKE task.

---
+++ 08354BICAKE.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    long long n, cuts, pieces;
+    while (cin >> n) {
+        cuts = 1;
+        pieces = 1;
+        while (pieces < n) {
+            pieces += cuts;
+            cuts++;
+        }
+        cout << cuts - 1 << endl;
+    }
+}
