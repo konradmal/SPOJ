5a06e41d7311140c4123f867bac27467ce7c619b
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-26 19:49:53 +0100

    Added 17140AL_13_10 task.

---
+++ 17140AL_13_10.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCaseCount;
+    cin >> testCaseCount;
+    string mainSequence, subSequence;
+    for (int i = 0; i < testCaseCount; i++) {
+        cin >> mainSequence >> subSequence;
+        int subSeqIndex = 0;
+        for (int j = 0; j < mainSequence.length() && subSeqIndex < subSequence.length(); j++) {
+            if (mainSequence[j] == subSequence[subSeqIndex]) {
+                subSeqIndex++;
+            }
+        }
+        if (subSeqIndex == subSequence.length())
+            cout << "Tak" << endl;
+        else
+            cout << "Nie" << endl;
+    }
+}
