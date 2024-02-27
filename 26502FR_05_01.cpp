de0e7a5959e1ba288043e5a862b6d7e596141a50
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-27 20:40:28 +0100

    Added 26502FR_05_01 task.

---
+++ 26502FR_05_01.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+	map<string, int> dayOfWeek = {
+		{"Pn", 0}, {"Wt", 1}, {"Sr", 2}, {"Cz", 3}, {"Pt", 4}, {"So", 5}, {"Nd", 6}
+	};
+	string days[] = { "Pn", "Wt", "Sr", "Cz", "Pt", "So", "Nd" };
+	int testCases;
+	cin >> testCases;
+	string currentDay;
+	int n;
+	for (int i = 0; i < testCases; i++) {
+		cin >> currentDay >> n;
+		cout << days[(dayOfWeek[currentDay] + n) % 7] << endl;
+	}
+}
