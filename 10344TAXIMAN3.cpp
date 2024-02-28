4d47ee76287e52ede7d88d557a84630e0efc90c8
konradmal <42766275+konradmal@users.noreply.github.com>
2024-02-28 13:01:29 +0100

    Added 10344TAXIMAN3 task.

---
+++ 10344TAXIMAN3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main() {
+    long dimensions, testCaseCount, coordinate, distanceSum;
+    cin >> dimensions >> testCaseCount;
+    for (int i = 0; i < testCaseCount; i++) {
+        vector<long> startPoint;
+        vector<long> endPoint;
+        distanceSum = 0;
+        for (int j = 0; j < dimensions; j++) {
+            cin >> coordinate;
+            startPoint.push_back(coordinate);
+        }
+        for (int j = 0; j < dimensions; j++) {
+            cin >> coordinate;
+            endPoint.push_back(coordinate);
+        }
+        for (int j = 0; j < dimensions; j++) {
+            distanceSum += abs(startPoint[j] - endPoint[j]);
+        }
+        cout << distanceSum << endl;
+    }
+}
