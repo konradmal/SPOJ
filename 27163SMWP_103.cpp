d55d46a4e9b22d89ec9f7571ed3617253338500f
konradmal <42766275+konradmal@users.noreply.github.com>
2024-03-04 21:23:25 +0100

    Added 27163SMWP_103 task.

---
+++ 27163SMWP_103.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int testCaseCount, number;
+    cin >> testCaseCount;
+    for(int i = 0; i < testCaseCount; i++) {
+        cin >> number;
+        if (number > 0 && pow((int)sqrt(number), 2) == number)
+            cout << "TAK" << endl;
+        else
+            cout << "NIE" << endl;
+    }
+}
