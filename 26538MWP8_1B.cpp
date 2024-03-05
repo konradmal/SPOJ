1c68b5e39a560ca3a6fd19987459ca7120c4ffb2
konradmal <42766275+konradmal@users.noreply.github.com>
2024-03-05 15:49:24 +0100

    Added 26538MWP8_1B task.

---
+++ 26538MWP8_1B.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+    vector<string> originalSongWords;
+    vector<string> rememberedSongWords;
+    string word;
+    while (true) {
+        cin >> word;
+        originalSongWords.push_back(word);
+        if (cin.peek() == '\n') 
+            break;
+    }
+    cin.ignore();
+    while (true) {
+        cin >> word;
+        rememberedSongWords.push_back(word);
+        if (cin.peek() == '\n') 
+            break;
+    }
+    for (string rememberedWord : rememberedSongWords) {
+        auto pos = find(originalSongWords.begin(), originalSongWords.end(), rememberedWord);
+        if (pos != originalSongWords.end())
+            originalSongWords.erase(pos);
+    }
+    sort(originalSongWords.begin(), originalSongWords.end());
+    cout << originalSongWords.size() << endl;
+    for (string word : originalSongWords) {
+        cout << word << endl;
+    }
+}
