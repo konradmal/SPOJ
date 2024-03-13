2efa1c5132892bb155e53e59addd1044a1585b04
konradmal <42766275+konradmal@users.noreply.github.com>
2024-03-13 11:33:03 +0100

    Added 09538KOTYYYYY task.

---
+++ 09538KOTYYYYY.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int testCases;
+    cin >> testCases;
+    int ageCat1, posCat1, ageCat2, posCat2;
+    for (int i = 0; i < testCases; i++) {
+        cin >> ageCat1 >> posCat1 >> ageCat2 >> posCat2;
+        if (ageCat1 > ageCat2)
+            if (int(abs(posCat2 - posCat1) + 1) % 3 == 2)
+                cout << 0 << endl;
+            else
+                cout << 1 << endl;
+        else
+            if (int(abs(posCat2 - posCat1) + 1) % 3 == 2)
+                cout << 1 << endl;
+            else
+                cout << 0 << endl;
+    }
+}
