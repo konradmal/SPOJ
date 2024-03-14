33c5543f9271f232315ec1c55c98674c7f1eb828
konradmal <42766275+konradmal@users.noreply.github.com>
2024-03-14 10:41:26 +0100

    Added 11059MWP4_1E task.

---
+++ 11059MWP4_1E.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int testCases, sequenceLength, element;
+    cin >> testCases;
+    int totalSum, prefixSum, suffixSum;
+    bool found;
+    for (int i = 0; i < testCases; i++) {
+        cin >> sequenceLength;
+        vector<int> sequenceElements;
+        totalSum = 0, prefixSum = 0, suffixSum = 0;
+        for (int j = 0; j < sequenceLength; j++) {
+            cin >> element;
+            sequenceElements.push_back(element);
+            totalSum += element;
+        }
+        suffixSum = totalSum;
+        found = false;
+        for (int j = 0; j < sequenceLength - 1; j++) {
+            prefixSum += sequenceElements[j];
+            suffixSum -= sequenceElements[j];
+            if (prefixSum == suffixSum) {
+                cout << j + 1 << endl;
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+            cout << 0 << endl;
+    }
+}
