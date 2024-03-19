168a2b49bcd0fe391091d471d84966b978be0ad7
konradmal <42766275+konradmal@users.noreply.github.com>
2024-03-19 15:15:42 +0100

    Added 17204AL_12_01 task.

---
+++ 17204AL_12_01.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main() {
+    int testCases, floors, currentPosition, lowestPosition, highestPosition;
+    cin >> testCases;
+    string sequence;
+    for (int test = 0; test < testCases; ++test) {
+        cin >> floors >> sequence;
+        currentPosition = 0;
+        lowestPosition = 0;
+        highestPosition = 0;
+        for (char move : sequence) {
+            if (move == 'D') {
+                currentPosition--;
+                lowestPosition = min(lowestPosition, currentPosition);
+            } else {
+                currentPosition++;
+                highestPosition = max(highestPosition, currentPosition);
+            }
+        }
+        if (highestPosition - lowestPosition < floors)
+            cout << "TAK" << endl;
+        else
+            cout << "NIE" << endl;
+    }
+}
