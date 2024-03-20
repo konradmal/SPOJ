789171af461a66a1c7d9a6f09853b60b3fbc7f96
konradmal <42766275+konradmal@users.noreply.github.com>
2024-03-20 12:25:54 +0100

    Added 18358AL_14_01 task.

---
+++ 18358AL_14_01.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double letterFrequencies[26] = {};
+    double totalLetters = 0;
+    string word;
+    while (cin >> word) {
+        for (char letter : word) {
+            letterFrequencies[(letter) - 'A']++;
+            totalLetters++;
+        }
+    }
+    for (int i = 0; i < 26; i++) {
+        float percentage = round((letterFrequencies[i] / totalLetters) * 100);
+        cout << char(i + 'A');
+        for (int j = 0; j < percentage; j++)
+            cout << "*";
+        cout << endl;
+    }
+}
