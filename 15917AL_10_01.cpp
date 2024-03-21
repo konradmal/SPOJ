aa9dfd762aa5bafee695e815706548c009b54fc6
konradmal <42766275+konradmal@users.noreply.github.com>
2024-03-21 09:59:51 +0100

    Added 15917AL_10_01 task.

---
+++ 15917AL_10_01.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCount, codeLength, unknownCount;
+    cin >> testCount;
+    string code;
+    for (int i = 0; i < testCount; i++) {
+        cin >> codeLength >> code;
+        unknownCount = 0;
+        if (codeLength == 1 && code[0] == '?') {
+            cout << 10 << endl;
+            continue;
+        }
+        if (codeLength == 1 && code[0] != '?') {
+            cout << 1 << endl;
+            continue;
+        }
+        cout << (code[0] == '?' ? 9 : 1);
+        for (int j = 1; j < code.length(); j++) {
+            if (code[j] == '?') {
+                unknownCount++;
+            }
+        }
+        for (int j = 0; j < unknownCount; j++) {
+            cout << 0;
+        }
+        cout << endl;
+    }
+}
