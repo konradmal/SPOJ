d15bd85ce036173d57bc9ee1b8233bbc2ef68234
konradmal <42766275+konradmal@users.noreply.github.com>
2024-03-25 16:04:48 +0100

    Added 31319FR_07_04 task.

---
+++ 31319FR_07_04.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int testCaseCount;
+    double firstDiscountPercent, secondDiscountPercent, finalPricePercent;
+    cin >> testCaseCount;
+    for (int i = 0; i < testCaseCount; i++) {
+        finalPricePercent = 100;
+        cin >> firstDiscountPercent >> secondDiscountPercent;
+        finalPricePercent -= firstDiscountPercent / 100 * finalPricePercent;
+        finalPricePercent -= secondDiscountPercent / 100 * finalPricePercent;
+        cout << fixed << setprecision(2) << 100 - finalPricePercent << endl;
+    }
+}
