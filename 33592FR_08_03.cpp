5b991cdd633bdbb41de34ed8771c7c97fd79d6d9
konradmal <42766275+konradmal@users.noreply.github.com>
2024-03-26 08:33:40 +0100

    Added 33592FR_08_03 task.

---
+++ 33592FR_08_03.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCount;
+    string fullPassword, partialPassword;
+    cin >> testCount;
+    bool match;
+    for (int i = 0; i < testCount; i++) {
+        match = true;
+        cin >> fullPassword >> partialPassword;
+        if (fullPassword.length() != partialPassword.length()) {
+            cout << "error" << endl;
+            continue;
+        }
+        for (int j = 0; j < fullPassword.length(); j++)
+            if (!(fullPassword[j] == '*' || partialPassword[j] == '*' || fullPassword[j] == partialPassword[j])) {
+                cout << "error" << endl;
+                match = false;
+                break;
+            }
+        if (match)
+            cout << "ok" << endl;
+    }
+}
