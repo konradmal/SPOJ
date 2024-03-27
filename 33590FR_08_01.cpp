7c5f1f757d0e7971a297d81a8f4068012ec90989
konradmal <42766275+konradmal@users.noreply.github.com>
2024-03-27 12:39:32 +0100

    Added 33590FR_08_01 task.

---
+++ 33590FR_08_01.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    long long number;
+    int digit, zeros = 0;
+    cin >> number;
+    bool hasEvenDigit = false;
+    if (number == 0)
+        cout << "Tak" << endl;
+    else {
+        while (number > 0) {
+            digit = number % 10;
+            if (digit % 2 == 0) {
+                cout << "Tak" << endl;
+                hasEvenDigit = true;
+                break;
+            }
+            number /= 10;
+        }
+        if (!hasEvenDigit)
+            cout << "Nie" << endl;
+    }
+}
