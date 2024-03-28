c6ed72dc85bb1f180825f8c079eaf17675b79bf7
konradmal <42766275+konradmal@users.noreply.github.com>
2024-03-28 09:02:54 +0100

    Added 26503FR_05_02 task.

---
+++ 26503FR_05_02.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int testCases;
+    cin >> testCases;
+    cin.ignore();
+    string password;
+    bool hasUpper, hasLower, hasDigit, hasSpecial;
+    for (int i = 0; i < testCases; i++) {
+        getline(cin, password);
+        hasUpper = false, hasLower = false, hasDigit = false, hasSpecial = false;
+        if (password.length() >= 8) {
+            for (char ch : password) {
+                if (isupper(ch)) 
+                    hasUpper = true;
+                else if (islower(ch)) 
+                    hasLower = true;
+                else if (isdigit(ch)) 
+                    hasDigit = true;
+                else hasSpecial = true;
+            }
+            if (hasUpper && hasLower && hasDigit && hasSpecial)
+                cout << password << endl;
+        }
+    }
+}
