2d2c4a271795c907143c72d0b41a264f701a8bfd
konradmal <42766275+konradmal@users.noreply.github.com>
2024-04-01 20:42:18 +0200

    Added 33591FR_08_02 task.

---
+++ 33591FR_08_02.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCases, winsStas, lossesStas, winsWies, lossesWies, winsGrzes, lossesGrzes, totalWins, totalLosses;
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        totalWins = 0, totalLosses = 0;
+        cin >> winsStas >> lossesStas >> winsWies >> lossesWies >> winsGrzes >> lossesGrzes;
+        totalWins += winsStas + winsWies + winsGrzes;
+        totalLosses += lossesStas + lossesWies + lossesGrzes;
+        cout << (totalWins + totalLosses) / 3 * 2 - totalWins << " " << (totalWins + totalLosses) / 3 * 2 - totalLosses << endl;
+    }
+}
