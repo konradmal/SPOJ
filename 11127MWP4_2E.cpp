2d4bbd4c94d49b46289a067aeca3e1d10a54d5e4
konradmal <42766275+konradmal@users.noreply.github.com>
2024-04-02 13:23:51 +0200

    Added 11127MWP4_2E task.

---
+++ 11127MWP4_2E.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int testCases;
+    cin >> testCases;
+    int numPeople, numMessages, initialSpeaker, sender, receiver;
+    for (int i = 0; i < testCases; i++) {
+        cin >> numPeople >> numMessages >> initialSpeaker;
+        vector<int> hasHeardMessage(numPeople + 1, 0);
+        hasHeardMessage[initialSpeaker] = 1;
+        cout << initialSpeaker;
+        for (int j = 0; j < numMessages; j++) {
+            cin >> sender >> receiver;
+            if (hasHeardMessage[sender] && !hasHeardMessage[receiver]) {
+                hasHeardMessage[receiver] = 1;
+                cout << " " << receiver;
+            }
+        }
+        cout << endl;
+    }
+}
