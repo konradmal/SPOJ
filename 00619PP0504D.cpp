11ba1d566451143e1457d302d9d13e73be88e747
konradmal <42766275+konradmal@users.noreply.github.com>
2024-04-03 11:27:47 +0200

    Added 00619PP0504D task.

--- 00619PP0504D.cpp
+++ 00619PP0504D.cpp
@@ -11,5 +11,6 @@ int main() {
         cin >> floatValue;
         for (int byteIndex = 3; byteIndex >= 0; byteIndex--)
             cout << hex << (int)(((const unsigned char*)&floatValue)[byteIndex]) << " ";
+        cout << endl;
     }
 }
