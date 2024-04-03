6c68140b5cfe6c8ec8113b1fcfcd0b7134d0a532
konradmal <42766275+konradmal@users.noreply.github.com>
2024-04-03 11:23:26 +0200

    Added 00619PP0504D task.

---
+++ 00619PP0504D.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	float n;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		for (int j = 3; j >= 0; j--) {
+			cout << hex << (int)(((const unsigned char*)&n)[j]) << " ";
+		}
+		cout << endl;
+	}
+}
