7fc95ff45136613cc6513b17bca10147af18dfa8
konradmal <42766275+konradmal@users.noreply.github.com>
2024-04-07 12:14:12 +0200

    Added 00806FANGEN task.

---
+++ 00806FANGEN.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int windmillOrder;
+    while (cin >> windmillOrder && windmillOrder != 0) {
+        if (windmillOrder == 1 || windmillOrder == -1)
+            cout << "**" << endl << "**" << endl;
+        else if (windmillOrder > 1) {
+            for (int row = 0; row < windmillOrder; row++) {
+                for (int col = 0; col <= row; col++)
+                    cout << "*";
+                for (int col = row + 1; col < windmillOrder; col++)
+                    cout << ".";
+                for (int col = row; col < windmillOrder; col++)
+                    cout << "*";
+                for (int col = row; col > 0; col--)
+                    cout << ".";
+                cout << endl;
+            }
+            for (int row = 0; row < windmillOrder; row++) {
+                for (int col = row + 1; col < windmillOrder; col++)
+                    cout << ".";
+                for (int col = 0; col <= row; col++)
+                    cout << "*";
+                for (int col = row; col > 0; col--)
+                    cout << ".";
+                for (int col = row; col < windmillOrder; col++)
+                    cout << "*";
+                cout << endl;
+            }
+        } else {
+            windmillOrder = -windmillOrder;
+            for (int row = 0; row < windmillOrder; row++) {
+                for (int col = row; col > 0; col--)
+                    cout << ".";
+                for (int col = row; col < windmillOrder; col++)
+                    cout << "*";
+                for (int col = row + 1; col < windmillOrder; col++)
+                    cout << ".";
+                for (int col = 0; col <= row; col++)
+                    cout << "*";
+                cout << endl;
+            }
+            for (int row = 0; row < windmillOrder; row++) {
+                for (int col = row; col < windmillOrder; col++)
+                    cout << "*";
+                for (int col = row; col > 0; col--)
+                    cout << ".";
+                for (int col = 0; col <= row; col++)
+                    cout << "*";
+                for (int col = row + 1; col < windmillOrder; col++)
+                    cout << ".";
+                cout << endl;
+            }
+        }
+        cout << endl;
+    }
+}
