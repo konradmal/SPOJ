68d4bb0da639915e9432a41fe2a3b7ceccf8b18a
konradmal <42766275+konradmal@users.noreply.github.com>
2024-04-10 13:31:14 +0200

    Added 42300FR_19_02 task.

---
+++ 42300FR_19_02.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	string symbol;
+	cin >> symbol;
+	if (symbol == "papier")
+		cout << "nozyce";
+	else if (symbol == "kamien")
+		cout << "papier";
+	else if (symbol == "nozyce")
+		cout << "kamien";
+	else
+		cout << "przegrales";
+}
