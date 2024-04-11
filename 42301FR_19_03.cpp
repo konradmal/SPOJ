2c0afe8f554fbac784699b8b206daf700d8a74ed
konradmal <42766275+konradmal@users.noreply.github.com>
2024-04-11 10:51:42 +0200

    Added 42301FR_19_03 task.

---
+++ 42301FR_19_03.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int numberOfCups, numberOfSwaps, swapTemp, firstCupIndex, secondCupIndex;
+    cin >> numberOfCups;
+    vector<int> cupsIDs;
+    for (int i = 1; i <= numberOfCups; i++)
+        cupsIDs.push_back(i);
+    cin >> numberOfSwaps;
+    for (int i = 0; i < numberOfSwaps; i++) {
+        cin >> firstCupIndex >> secondCupIndex;
+        swapTemp = cupsIDs[firstCupIndex - 1];
+        cupsIDs[firstCupIndex - 1] = cupsIDs[secondCupIndex - 1];
+        cupsIDs[secondCupIndex - 1] = swapTemp;
+    }
+    for (int i = 0; i < numberOfCups; i++)
+        cout << cupsIDs[i] << " ";
+}
