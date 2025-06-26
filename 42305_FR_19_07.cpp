916c7deea7895bc1bcaa18d0afe54c521834d00c
konradmal <42766275+konradmal@users.noreply.github.com>
2025-06-26 17:15:47 +0200

    Added 42305_FR_19_07 task.

---
+++ 42305_FR_19_07.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int numberOfLines;
+    cin >> numberOfLines;
+    int wordsInLine;
+    string word;
+    for (int lineIndex = 0; lineIndex < numberOfLines; lineIndex++)
+    {
+        cin >> wordsInLine;
+        for (int wordIndex = 0; wordIndex < wordsInLine; wordIndex++)
+        {
+            cin >> word;
+            if (word != ":)" && word != ":(" && word != ";)" && word != ":D" &&
+                word != ":P" && word != "xD" && word != ":>" && word != "<3")
+            {
+                cout << word << " ";
+            }
+        }
+        cout << endl;
+    }
+}
