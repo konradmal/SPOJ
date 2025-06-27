024ea9753b62c846bc0534360e2e897f792625f8
konradmal <42766275+konradmal@users.noreply.github.com>
2025-06-27 21:41:59 +0200

    Added 42512_FR_20_05 task.

---
+++ 42512_FR_20_05.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int bajtekLegs = 0, bajtekHeads = 0;
+    int frajtekLegs = 0, frajtekHeads = 0;
+    int animals;
+    cin >> animals;
+    bajtekLegs += 2 * animals;
+    bajtekHeads += animals;
+    cin >> animals;
+    bajtekLegs += 4 * animals;
+    bajtekHeads += animals;
+    cin >> animals;
+    frajtekLegs += 2 * animals;
+    frajtekHeads += animals;
+    cin >> animals;
+    frajtekLegs += 4 * animals;
+    frajtekHeads += animals;
+    if ((bajtekLegs > frajtekLegs) || ((bajtekLegs == frajtekLegs && bajtekHeads > frajtekHeads)))
+        cout << "Bajtek";
+    else if ((bajtekLegs < frajtekLegs) || ((bajtekLegs == frajtekLegs && bajtekHeads < frajtekHeads)))
+        cout << "Frajtek";
+    else
+        cout << "remis";
+}
