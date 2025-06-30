6b6c851cc7fa94aeb6becd039c41fdf2b06ca3bc
konradmal <42766275+konradmal@users.noreply.github.com>
2025-06-30 19:02:40 +0200

    Added 42511_FR_20_04 task.

---
+++ 42511_FR_20_04.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int numberA, numberB;
+    cin >> numberA >> numberB;
+    cout << "Witaj na V Konkursie FRAKTAL! Suma " << numberA << " i " << numberB << " to " << numberA + numberB << "." << endl;
+    cout << "Powodzenia, programisto!";
+}
