0782c7e88614be8a7d9b401ef5217f753567389c
konradmal <42766275+konradmal@users.noreply.github.com>
2025-07-01 21:42:12 +0200

    Added 42304_FR_19_06 task.

---
+++ 42304_FR_19_06.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int exponent;
+    cin >> exponent;
+    if (exponent != 0)
+        cout << "TAK";
+    else
+        cout << "NIE";
+}
