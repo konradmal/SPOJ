128a3ff6f6e08f1cd09113fb2bbed86d17cf15b7
konradmal <42766275+konradmal@users.noreply.github.com>
2025-07-02 19:51:57 +0200

    Added 42303_FR_19_05 task.

---
+++ 42303_FR_19_05.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main() {
+    int testCount, firstDigit, lastDigit;
+    cin >> testCount;
+    string inputString;
+    for (int i = 0; i < testCount; i++)
+    {
+        firstDigit = -1, lastDigit = -1;
+        cin >> inputString;
+        for (int j = 0; j < inputString.length(); j++)
+        {
+            if (isdigit(inputString[j]))
+            {
+                if (firstDigit == -1)
+                {
+                    firstDigit = int(inputString[j]) - 48;
+                }
+                else
+                {
+                    lastDigit = int(inputString[j]) - 48;
+                }
+            }
+        }
+        if (firstDigit > -1 && lastDigit > -1)
+            cout << firstDigit << " " << lastDigit << endl;
+        else
+            cout << "BRAK" << endl;
+    }
+}
