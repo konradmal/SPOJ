7f13c42593956e31b02310366df2393eeed6789d
konradmal <42766275+konradmal@users.noreply.github.com>
2025-07-03 19:28:08 +0200

    Added 42302_FR_19_04 task.

---
+++ 42302_FR_19_04.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int syllableCount;
+    cin >> syllableCount;
+    string currentSyllable, previousSyllable;
+    cin >> currentSyllable;
+    for (int i = 0; i < syllableCount - 1; i++)
+    {
+        previousSyllable = currentSyllable;
+        cin >> currentSyllable;
+        if (currentSyllable == "ma" && previousSyllable == "ma")
+        {
+            cout << "mama";
+            break;
+        }
+        else if (currentSyllable == "ta" && previousSyllable == "ta")
+        {
+            cout << "tata";
+            break;
+        }
+    }
+}
