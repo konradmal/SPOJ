bc7dc617df5a65f21afa5f8252dd3bed8b06200f
konradmal <42766275+konradmal@users.noreply.github.com>
2025-07-06 17:09:43 +0200

    Added 42225_FR_18_03 task.

---
+++ 42225_FR_18_03.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int boardLengths[4];
+    for (int i = 0; i < 4; i++) {
+        cin >> boardLengths[i];
+    }
+    sort(boardLengths, boardLengths + 4);
+    cout << boardLengths[0] * boardLengths[2];
+}
