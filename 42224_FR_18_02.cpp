d2cff07d6b1fbc96b6148655df70c1366b6f6bb1
konradmal <42766275+konradmal@users.noreply.github.com>
2025-07-06 17:15:12 +0200

    Added 42224_FR_18_02 task.

---
+++ 42224_FR_18_02.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double netAmount, grossAmount;
+    cin >> netAmount >> grossAmount;
+    double vatAmount = grossAmount - netAmount;
+    double deductibleVAT = vatAmount / 2.0;
+    double taxDeductibleCost = netAmount + deductibleVAT;
+    cout << fixed << setprecision(2) << deductibleVAT << " " << taxDeductibleCost;
+}
