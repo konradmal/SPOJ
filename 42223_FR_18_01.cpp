dd69641738d3540eac4dd99e00964a9b1dc34b47
konradmal <42766275+konradmal@users.noreply.github.com>
2025-07-06 17:22:50 +0200

    Added 42223_FR_18_01 task.

---
+++ 42223_FR_18_01.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int testCount, dayNumber;
+    cin >> testCount;
+    for (int i = 0; i < testCount; i++) {
+        cin >> dayNumber;
+        int coordinatorIndex = (dayNumber - 1) / 7 % 6;
+        if (coordinatorIndex == 0)
+            cout << "DAWID" << endl;
+        else if (coordinatorIndex == 1)
+            cout << "GRZESIEK" << endl;
+        else if (coordinatorIndex == 2)
+            cout << "MACIEK" << endl;
+        else if (coordinatorIndex == 3)
+            cout << "MARCIN" << endl;
+        else if (coordinatorIndex == 4)
+            cout << "MARIUSZ" << endl;
+        else
+            cout << "RAFAL" << endl;
+    }
+}
