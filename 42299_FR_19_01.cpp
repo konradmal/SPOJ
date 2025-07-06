99efee2145a9cc62fe045c899498a9f5bb984fe1
konradmal <42766275+konradmal@users.noreply.github.com>
2025-07-06 17:04:41 +0200

    Added 42299_FR_19_01 task.

---
+++ 42299_FR_19_01.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int participantsFromCity, totalMeals = 5;
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> participantsFromCity;
+        totalMeals += participantsFromCity;
+    }
+    cout << totalMeals;
+}
