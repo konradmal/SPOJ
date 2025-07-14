922a05114ffd00a10329a7e18c37f55a271b7224
konradmal <42766275+konradmal@users.noreply.github.com>
2025-07-14 12:49:45 +0200

    Added 41966_FR_17_04 task.

---
+++ 41966_FR_17_04.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#define M_PI 3.14159265358979323846
+using namespace std;
+
+int main() {
+    int testCount;
+    cin >> testCount;
+    for (int test = 0; test < testCount; test++) {
+        int maciekPizzaCount, maciekDiameter, grzesiekPizzaCount, grzesiekDiameter;
+        cin >> maciekPizzaCount >> maciekDiameter >> grzesiekPizzaCount >> grzesiekDiameter;
+        double maciekArea = maciekPizzaCount * M_PI * (maciekDiameter * maciekDiameter) / 4.0;
+        double grzesiekArea = grzesiekPizzaCount * M_PI * (grzesiekDiameter * grzesiekDiameter) / 4.0;
+        if (maciekArea > grzesiekArea)
+            cout << "Maciek" << endl;
+        else if (maciekArea < grzesiekArea)
+            cout << "Grzesiek" << endl;
+        else
+            cout << "porcje sa takie same" << endl;
+    }
+}
