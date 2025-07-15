d2a948a1e60dc40c41b4b38d2f360f88e00f7528
konradmal <42766275+konradmal@users.noreply.github.com>
2025-07-15 15:32:52 +0200

    Added 41964_FR_17_02 task.

---
+++ 41964_FR_17_02.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int maciekTemp, desiredAvg;
+    cin >> maciekTemp >> desiredAvg;
+    int grzesiekTemp = 2 * desiredAvg - maciekTemp;
+    if (grzesiekTemp >= 15 && grzesiekTemp <= 30)
+        cout << grzesiekTemp;
+    else
+        cout << "NIE DA SIE";
+}
