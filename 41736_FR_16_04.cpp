c35b9771d729bcb62265fcae9b4700d987f87138
konradmal <42766275+konradmal@users.noreply.github.com>
2025-07-16 19:16:16 +0200

    Added 41736_FR_16_04 task.

---
+++ 41736_FR_16_04.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int eggCount;
+    cin >> eggCount;
+    if (eggCount < 1000)
+    {
+        double discountRate = (10 - eggCount / 100) / 10.;
+        cout << (1 - discountRate) * 0.5 * eggCount;
+    }
+    else
+        cout << eggCount - 500;
+}
