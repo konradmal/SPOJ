f0b57d6a1e7c1be42f5d8d3c52bacfb845308033
konradmal <42766275+konradmal@users.noreply.github.com>
2025-07-17 17:20:57 +0200

    Added 41733_FR_16_01 task.

---
+++ 41733_FR_16_01.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int distanceLeft, distanceRight, dayOfYear;
+    cin >> distanceLeft >> distanceRight >> dayOfYear;
+    if (dayOfYear == 33)
+        cout << "WRACAJ";
+    else if (distanceLeft < distanceRight)
+        cout << "W LEWO";
+    else
+        cout << "W PRAWO";
+}
