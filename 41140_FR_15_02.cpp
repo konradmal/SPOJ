44039aa3fba36ba639e1c09d029e3e25f6b9a665
konradmal <42766275+konradmal@users.noreply.github.com>
2025-07-17 17:34:46 +0200

    Added 41140_FR_15_02 task.

---
+++ 41140_FR_15_02.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    int numberOfPeople, buildingNumber;
+    string surname;
+    set<int> occupiedBuildings;
+    cin >> numberOfPeople;
+    for (int i = 0; i < numberOfPeople; i++)
+    {
+        cin >> buildingNumber >> surname;
+        occupiedBuildings.insert(buildingNumber);
+    }
+    cout << occupiedBuildings.size() << endl;
+}
