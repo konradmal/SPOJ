a786520336d0f82bc3723970c2155b164035f26f
konradmal <42766275+konradmal@users.noreply.github.com>
2025-07-17 17:14:49 +0200

    Added 41734_FR_16_02 task.

---
+++ 41734_FR_16_02.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int prev, curr, next, sampleCount, peakCount = 0;
+    cin >> sampleCount;
+    cin >> prev >> curr >> next;
+    if (prev < curr && curr > next)
+        peakCount++;
+    for (int i = 0; i < sampleCount - 3; i++)
+    {
+        prev = curr;
+        curr = next;
+        cin >> next;
+        if (prev < curr && curr > next)
+            peakCount++;
+    }
+    cout << peakCount;
+}
