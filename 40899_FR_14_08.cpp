37a7205c165214132dcd3d8c688f87f1142fc3e0
konradmal <42766275+konradmal@users.noreply.github.com>
2025-07-17 17:35:49 +0200

    Added 40899_FR_14_08 task.

---
+++ 40899_FR_14_08.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int inputNumber, square;
+    cin >> inputNumber;
+    square = sqrt(inputNumber);
+    square = square * square;
+    if (inputNumber == square)
+        cout << "TAK";
+    else
+        cout << "NIE";
+}
