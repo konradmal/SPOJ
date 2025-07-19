2f036ef51de0eb25ce8c6b5ea5dba9e278b64347
konradmal <42766275+konradmal@users.noreply.github.com>
2025-07-20 01:42:55 +0200

    Added 40897_FR_14_06 task.

---
+++ 40897_FR_14_06.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int stickCountByLength[257] = {};
+    int totalSticks, stickLength;
+    cin >> totalSticks;
+    for (int i = 0; i < totalSticks; i++)
+    {
+        cin >> stickLength;
+        stickCountByLength[stickLength]++;
+    }
+    int triangles = 0;
+    for (int i = 1; i < 257; i++)
+    {
+        triangles += stickCountByLength[i] / 3;
+    }
+    cout << triangles;
+}
