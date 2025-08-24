7f2102e889fa5949c999722614391930772b953f
konradmal <42766275+konradmal@users.noreply.github.com>
2025-08-24 12:50:02 +0200

    Added 40496_DDZ_02_03 task.

---
+++ 40496_DDZ_02_03.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string digits;
+    cin >> digits;
+    int firstZero = digits.find('0');
+    int lastOne   = digits.rfind('1');
+    if (lastOne <= firstZero + 1)
+        cout << "PUSTY";
+    else
+        cout << digits.substr(firstZero + 1, lastOne - firstZero - 1);
+}
